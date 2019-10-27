5d951457f8c4db9b8d2e48e131a6f400c1286fa5
toshyss <toshy.ss@gmail.com>
2019-10-27 23:44:54 +0900

    created main and hello controller

---
+++ src/main/java/io/pivotal/pal/tracker/review/PalTrackerApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
+}
