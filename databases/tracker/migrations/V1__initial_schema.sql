1491b0d14b2034931d98b838becae35ae20408c3
toshyss <toshy.ss@gmail.com>
2019-12-25 19:39:02 +0900

    clean up comments in sql files

--- databases/tracker/migrations/V1__initial_schema.sql
+++ databases/tracker/migrations/V1__initial_schema.sql
@@ -1,15 +1,3 @@
--- CREATE TABLE time_entries (
---   id         BIGINT(20) NOT NULL AUTO_INCREMENT,
---   project_id BIGINT(20),
---   user_id    BIGINT(20),
---   date       DATE,
---   hours      INT,
---
---   PRIMARY KEY (id)
--- )
---   ENGINE = innodb
---   DEFAULT CHARSET = utf8;
-
 CREATE TABLE time_entries (
   id         BIGINT(20) NOT NULL AUTO_INCREMENT,
   project_id BIGINT(20),
